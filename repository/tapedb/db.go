3fba5e8afb49cf48bdae924e0ec75554890ab5d5
wangminxiang <ryougi.nevermore@hotmail.com>
2022-08-09 17:05:10 +0800

    mod db

--- repository/tapedb/db.go
+++ repository/tapedb/db.go
@@ -1,4 +1,18 @@
 package tapedb
 
+import (
+	"github.com/dgraph-io/ristretto"
+	"os"
+	"sync"
+)
+
 type DB struct {
+	meta                 tapeMeta
+	file                 *os.File
+	blockCapacity        uint64
+	blockContentCapacity uint64
+	counter              sync.WaitGroup
+	cache                *ristretto.Cache
+	latestSeq            *sequence
+	closed               int64
 }
